f5347028a3810a8eabcaedf7090e6e44b1c2c072
Raiden-56 <67718820+Raiden-56@users.noreply.github.com>
2021-11-15 17:10:34 +0100

    Refonte :)

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-/*Réponse A */
-document.getElementById('A').addEventListener('click', A)
-function A(){
-var x = document.getElementById("answer");
-x.style.color = 'green'
-x.innerHTML = "✔️ Votre réponse est correcte !";
-}
- /*Réponse B*/
- document.getElementById('B').addEventListener('click', B)
-function B(){
-var x = document.getElementById("answer");
-x.style.color = 'red'
-x.innerHTML = "Votre réponse est incorrecte !";
-}
-/*Réponse C*/ 
-document.getElementById('C').addEventListener('click', C)
-function C(){
-var x = document.getElementById("answer");
-x.style.color = 'red'
-x.innerHTML = "Votre réponse est incorrecte !";
-}
-/*QUIZ 2 */
-/*Réponse A */
-document.getElementById('AA').addEventListener('click', AA)
-function AA(){
-    var x = document.getElementById("response");
-x.style.color = 'red'
-x.innerHTML = "Votre réponse est incorrecte !";
+const container = document.getElementById("container");
 
+const game = new class Game {
+    
+    constructor (){
+        this.question = 0;
+        this.init();
+    }
+
+    init () {
+        container.innerHTML += this.quizHTML(0, data[0].question, data[0].reponses);
+    }
+
+    next() {
+        this.question++;
+        if (this.question === data.length) return location.reload();
+        container.innerHTML += this.quizHTML(this.question, data[this.question].question, data[this.question].reponses);
+        setTimeout(() => {
+            container.scrollTo(this.question * document.getElementById("quiz0").clientWidth, 0);
+        }, 250);
+    }
+
+    wrong(id) {
+        document.getElementById(id).style.animationPlayState = "running";
+    }
+
+    quizHTML(number, question, answers) {
+        return `
+            <div id="quiz${number}" class="quiz quiz-${number}">
+                <div class="progress"></div>
+                <div class="question">${question}</div>
+                <div class="answers">
+                    <div id="answer${number}1" onClick="${answers[0].isTrue ? "game.next()" : `game.wrong('answer${number}1')`}" class="answer"><span class="markdown">A.</span> ${answers[0].text}</div>
+                    <div id="answer${number}2" onClick="${answers[1].isTrue ? "game.next()" : `game.wrong('answer${number}2')`}" class="answer"><span class="markdown">B.</span> ${answers[1].text}</div>
+                    <div id="answer${number}3" onClick="${answers[2].isTrue ? "game.next()" : `game.wrong('answer${number}3')`}" class="answer"><span class="markdown">C.</span> ${answers[2].text}</div>
+                </div>
+            </div>
+        `
+    }
 }
-/*Réponse B */
-document.getElementById('AB').addEventListener('click', AB)
-function AB(){
-var x = document.getElementById("response");
-x.style.color = 'red'
-x.innerHTML = "Votre réponse est incorrecte !";
-}
-/*Réponse C */
-document.getElementById('AC').addEventListener('click', AC)
-function AC(){
-    var x = document.getElementById("response");
-    x.style.color = 'green'
-    x.innerHTML = "✔️ Votre réponse est correcte !";
+
+function togglePopup() {
+    document.getElementById("popup").style.display = "none";
 }
