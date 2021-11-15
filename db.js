83e15e9b9fc576d19b53859e1fdad987404a7a2c
Raiden-56 <67718820+Raiden-56@users.noreply.github.com>
2021-11-15 17:11:26 +0100

    Refonte :)

---
+++ db.js
@@ -0,0 +1,35 @@
+var data = [{
+        "question": "Qu'est ce que le javascript ?",
+        "reponses": [
+            {
+                "text": "Langage de programmation qui permet d’implémenter des mécanismes complexes sur une page web.",
+                "isTrue": true,
+            },
+            {
+                "text": "Langage de balisage utilisé pour la création de pages web, permettant notamment de définir des liens hypertextes.8",
+                "isTrus": false
+            },
+            {
+                "text": "Un langage informatique qui décrit la présentation des documents HTML et XML.",
+                "isTrue": false,
+            }
+        ]
+    },
+    {
+        "question": "A quoi sert le language Javascript ?",
+        "reponses": [
+            {
+                "text": "Sert à mettre en forme les différents contenus définis par le HTML en leur appliquant des styles.",
+                "isTrue": false
+            },
+            {
+                "text": "C'est l'un des langages de programmation les plus polyvalents et les plus modernes au monde.",
+                "isTrue": false
+            },
+            {
+                "text": "Améliorer l'ergonomie d'un site Internet et/ou d'une interface applicative utilisateur.",
+                "isTrue": true
+            }
+        ]
+    }
+]
